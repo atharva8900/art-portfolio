1f8093a95902df3d2b101e3f41ac91c60b8cd478
atharva8900 <atharva8900@gmail.com>
2026-03-19 17:07:10 +0530

    Cleanup: remove debug console.log from UserProfile

--- src/components/auth/UserProfile.tsx
+++ src/components/auth/UserProfile.tsx
@@ -28,7 +28,6 @@ export default function UserProfile() {
     }
 
     const user = session?.user;
-    if (user?.image) console.log('User Profile Image URL:', user.image);
 
     if (!user) {
         return (
